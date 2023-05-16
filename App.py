c1f8f3e80a903f30dcd23fdfaabcc86f050c577b
Jafer010101 <77471880+Jafer010101@users.noreply.github.com>
2023-05-15 19:59:41 -0600

    Update App.py

--- App.py
+++ App.py
@@ -16,7 +16,7 @@ def cargar_datos():
 
 @app.route('/')
 def home():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 @app.route('/contacto', methods=['GET', 'POST'])
 def contacto():
